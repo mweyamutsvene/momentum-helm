b27dbe93db3be72ee5d71b64859376336a17f755
David Rasch <rasch@raschnet.com>
2021-09-08 15:41:52 -0400

    feat(day2): tictactoe component created

--- linkme-david/src/app/app.module.ts
+++ linkme-david/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TictactoeComponent } from './tictactoe/tictactoe.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TictactoeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
