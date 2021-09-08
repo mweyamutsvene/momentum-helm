94fb78059f85af8110294fb4eebef98446df8e9d
TommyHanks <tommy@crowdkout.com>
2021-09-08 13:22:44 -0600

    feat: tic tac toe game

--- linkme-tommy/src/app/app.component.ts
+++ linkme-tommy/src/app/app.component.ts
@@ -1,34 +1,157 @@
 import { Component, OnInit } from '@angular/core';
-import Profile, { profiles } from './profile.model';
+import Board, {Square} from './board.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent implements OnInit {
-  title = 'linkme-david';
-  profiles: Profile[] = profiles;
-
+  board!: Board;
+  turn: boolean = false
   ngOnInit() {
-    setTimeout(() => {
-      this.firstProfileSet = 'Bob"&<head>';
-      this.profiles[1].firstName = 'Thomas';
-    }, 3000);
+    let squares:Square[][] = [[Square.none, Square.none, Square.none],
+                              [Square.none, Square.none, Square.none],
+                              [Square.none, Square.none, Square.none]]  
+    this.board = new Board(squares)
   }
 
-  firstProfile() {
-    return this.profiles[0];
+  gameOver(){
+    let value = this.findWinner(this.board.board,3)
+    switch (value) {
+      case 0: {
+        return "Winner is X"
+      }
+      case 1: {
+        return "Winner is O"
+      }
+      default: {
+        if(this.boardIsFull(this.board.board)) {
+          return "You're Both Losers!"
+        }
+        return " "
+      }
+    }
   }
 
-  get firstProfileGet() {
-    return this.firstProfile();
-  }
-  set firstProfileSet(value: string) {
-    this.firstProfile().firstName = value;
+  onClick(row:number, column:number) {
+    if(this.turn == false) {
+      this.markSquare(row,column,Square.o)
+    }
+    else {
+      this.markSquare(row,column,Square.x)
+    }
+    this.turn = !this.turn
   }
 
-  onClick() {
-    this.firstProfileSet = 'Atul';
+  squareAt(row:number, column:number) {
+    let value = this.board.board[row][column];
+    switch (value) {
+      case 0: {
+        return "X"
+      }
+      case 1: {
+        return "O"
+      }
+      default: {
+        return "_"
+      }
+    }
+  }
+
+  markSquare(row:number,column:number, value: Square) {
+    this.board.board[row][column] = value
+    let winner = this.findWinner(this.board.board, 3)
+    return winner
+  }
+
+  boardIsFull(board: Square[][]) {
+    let isFull = true;
+
+    full: for (let row = 0; row < board.length; ++row) {
+        for (let col = 0; col < board.length; ++col) {
+            if (board[row][col] == Square.none) {
+                isFull = false;
+                break full;
+            }
+        }
+    }
+    return isFull
+  }
+
+  findWinner(board: Square[][],lengthToWin: number) {
+    // Check each lengthToWin x lengthToWin board for a winner.    
+    for (let top = 0; top <= board.length - lengthToWin; ++top) {
+        let bottom = top + lengthToWin - 1;
+
+        for (let left = 0; left <= board.length - lengthToWin; ++left) {
+            let right = left + lengthToWin - 1;
+
+            // Check each row.
+            nextRow: for (let row = top; row <= bottom; ++row) {
+                if (board[row][left] == Square.none) {
+                    continue;
+                }
+
+                for (let col = left; col <= right; ++col) {
+                    if (board[row][col] != board[row][left]) {
+                        continue nextRow;
+                    }
+                }
+
+                return board[row][left];
+            }
+
+            // Check each column.
+            nextCol: for (let col = left; col <= right; ++col) {
+                if (board[top][col] == Square.none) {
+                    continue;
+                }
+
+                for (let row = top; row <= bottom; ++row) {
+                    if (board[row][col] != board[top][col]) {
+                        continue nextCol;
+                    }
+                }
+
+                return board[top][col];
+            }
+
+            // Check top-left to bottom-right diagonal.
+            diag1: if (board[top][left] != Square.none) {
+                for (let i = 1; i < lengthToWin; ++i) {
+                    if (board[top+i][left+i] != board[top][left]) {
+                        break diag1;
+                    }
+                }
+
+                return board[top][left];
+            }
+
+            // Check top-right to bottom-left diagonal.
+            diag2: if (board[top][right] != Square.none) {
+                for (let i = 1; i < lengthToWin; ++i) {
+                    if (board[top+i][right-i] != board[top][right]) {
+                        break diag2;
+                    }
+                }
+
+                return board[top][right];
+            }
+        }
+    }    
+
+    // The board is full.
+    if (this.boardIsFull(board)) {
+        return Square.none;
+    }
+    // The board is not full and we didn't find a solution.
+    else {
+        return null;
+    }
   }
 }
+
+
+
---
+++ linkme-tommy/src/app/board.model.ts
@@ -0,0 +1,13 @@
+
+export enum Square {
+    x,
+    o,
+    none,
+}
+export default class Board {
+    constructor(
+      public board: Square[][],
+    ) {}
+  }
+  
+  
