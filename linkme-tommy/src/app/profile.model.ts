45c19d81ad838d72058cb681e04135cde87b1fdf
TommyHanks <tommy@crowdkout.com>
2021-09-08 10:52:47 -0600

    chore: copy over davids files to my directory

--- linkme-tommy/src/app/profile.model.ts
+++ linkme-tommy/src/app/profile.model.ts
@@ -1,4 +1,4 @@
-class Profile {
+export default class Profile {
   constructor(
     public firstName: string,
     public lastName: string,
@@ -6,3 +6,12 @@ class Profile {
     public experience: string[]
   ) {}
 }
+
+export const profiles = [
+  new Profile('David', 'Rasch', 'Mr.', [
+    'Developer 2020',
+    'Angular Instructor 2021',
+  ]),
+  new Profile('Alan', 'Cox', 'Mr.', ['CTO 2020-2021']),
+  new Profile('Dee', 'Meyers', 'Ms.', ['Student 2020', 'Developer 2021']),
+];
