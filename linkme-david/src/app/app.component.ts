c233c622d64a440f112e904aab4a3d09765dfba6
David Rasch <rasch@raschnet.com>
2021-09-08 11:59:58 -0400

    feat(day2): added template and component properties

--- linkme-david/src/app/app.component.ts
+++ linkme-david/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Profile, { profiles } from './profile.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'linkme-david';
+  profiles: Profile[] = profiles;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.profiles[0].firstName = 'Bob';
+      this.profiles[1].firstName = 'Thomas';
+    }, 3000);
+  }
+
+  firstProfile() {
+    return this.profiles[0];
+  }
 }
