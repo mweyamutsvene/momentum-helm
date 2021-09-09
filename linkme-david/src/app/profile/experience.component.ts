6622b0b20d25a9fcc3d7bb52bca12471ad78cf9e
David Rasch <rasch@raschnet.com>
2021-09-09 17:44:52 -0400

    feat(day3): angular ContentChild working now

--- linkme-david/src/app/profile/experience.component.ts
+++ linkme-david/src/app/profile/experience.component.ts
@@ -1,4 +1,11 @@
-import { ElementRef, Component, ContentChild, Input, OnInit, AfterContentInit } from '@angular/core';
+import {
+  ElementRef,
+  Component,
+  ContentChild,
+  Input,
+  OnInit,
+  AfterContentInit,
+} from '@angular/core';
 import { ProfilesService } from '../profiles.service';
 
 @Component({
@@ -6,22 +13,22 @@ import { ProfilesService } from '../profiles.service';
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss'],
 })
-export class ExperienceComponent implements OnInit, AfterContentInit{
+export class ExperienceComponent implements OnInit, AfterContentInit {
   @Input() profile!: number;
 
-  @ContentChild('[morestuff]') morestuff!: ElementRef;
+  @ContentChild('morestuff') morestuff!: ElementRef;
 
   newExperience: string = '';
 
   constructor(public profilesService: ProfilesService) {}
 
   ngOnInit() {
-    console.log(" ng on init");
+    console.log(' ng on init');
     console.log(this.morestuff);
   }
 
   ngAfterContentInit() {
-    console.log(" ng after content init");
+    console.log(' ng after content init');
     console.log(this.morestuff);
   }
 
