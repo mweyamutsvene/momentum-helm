4772dff433a9d1f42571553f9dbe0675b27691b5
David Rasch <rasch@raschnet.com>
2021-09-09 17:19:31 -0400

    feat(day3): ng-content demo

--- linkme-david/src/app/profile/experience.component.ts
+++ linkme-david/src/app/profile/experience.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { ElementRef, Component, ContentChild, Input, OnInit, AfterContentInit } from '@angular/core';
 import { ProfilesService } from '../profiles.service';
 
 @Component({
@@ -6,12 +6,24 @@ import { ProfilesService } from '../profiles.service';
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss'],
 })
-export class ExperienceComponent {
+export class ExperienceComponent implements OnInit, AfterContentInit{
   @Input() profile!: number;
+
+  @ContentChild('[morestuff]') morestuff!: ElementRef;
 
   newExperience: string = '';
 
   constructor(public profilesService: ProfilesService) {}
+
+  ngOnInit() {
+    console.log(" ng on init");
+    console.log(this.morestuff);
+  }
+
+  ngAfterContentInit() {
+    console.log(" ng after content init");
+    console.log(this.morestuff);
+  }
 
   get experience() {
     return this.profilesService.getProfile(this.profile).experience;
