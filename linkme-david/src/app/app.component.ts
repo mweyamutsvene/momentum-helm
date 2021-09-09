1da2f0de916e436b3730790975d52d647453c5be
David Rasch <rasch@raschnet.com>
2021-09-09 10:21:53 -0400

    feat(day3): clean up code to prepare for building the profile UI

--- linkme-david/src/app/app.component.ts
+++ linkme-david/src/app/app.component.ts
@@ -1,34 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import Profile, { profiles, Direction } from './profile.model';
+import { Component } from '@angular/core';
+import Profile, { profiles } from './profile.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'linkme-david';
   profiles: Profile[] = profiles;
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.firstProfileSet = 'Bob&<head>';
-      this.profiles[1].firstName = 'Thomas';
-    }, 3000);
-  }
-
-  firstProfile() {
-    return this.profiles[0];
-  }
-
-  get firstProfileGet() {
-    return this.firstProfile();
-  }
-  set firstProfileSet(value: string) {
-    this.firstProfile().firstName = value;
-  }
-
-  onClick() {
-    this.firstProfileSet = 'Atul';
-  }
 }
