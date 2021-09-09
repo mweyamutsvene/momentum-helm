49d2b9ab3fafe19f5001fde53e199bfc2fb2649e
David Rasch <rasch@raschnet.com>
2021-09-09 16:07:27 -0400

    feat(day3): lifecycle examples

--- linkme-david/src/app/profile/experience.component.ts
+++ linkme-david/src/app/profile/experience.component.ts
@@ -1,17 +1,36 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  ElementRef,
+  Input,
+  Output,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss'],
 })
-export class ExperienceComponent {
+export class ExperienceComponent implements OnDestroy {
   @Input() experience!: string[];
   @Output() newExperienceEvent = new EventEmitter<string>();
 
+  @ViewChild('myLocalRef') myLocalRef!: ElementRef;
+
   newExperience: string = '';
 
+  ngOnDestroy() {
+    console.log('destroy');
+    // unsubscribe
+    // cancel http request
+  }
+
   onNewExperience() {
+    console.log(this.myLocalRef.nativeElement);
+
+    console.log(this.myLocalRef);
     if (this.newExperience) {
       this.newExperienceEvent.emit(this.newExperience);
     }
--- linkme-david/src/app/profile/profile.component.ts
+++ linkme-david/src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges, OnDestroy } from '@angular/core';
 import Profile from '../profile.model';
 
 @Component({
