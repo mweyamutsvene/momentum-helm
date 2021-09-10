bd4cb1dfdf0bf57ce1ceb11d880135d7d1bdf07d
David Rasch <rasch@raschnet.com>
2021-09-10 09:46:07 -0400

    feat(day4): graphql notes update with exercises

--- linkme-david/src/app/profile/profile.component.ts
+++ linkme-david/src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, SimpleChanges, OnDestroy } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ProfilesService } from '../profiles.service';
 
 @Component({
